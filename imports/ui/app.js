25678714df1d18857786bab604854f1a7609ef1f
Muhamad Irham Prasetyo <irhamputraprasetyo@gmail.com>
2018-01-29 23:41:08 +0100

    Initial commit

---
+++ imports/ui/app.js
@@ -0,0 +1,21 @@
+import React from "react";
+import gql from "graphql-tag";
+import {graphql} from "react-apollo";
+
+const App = ({data}) => {
+    return(
+        <div>
+            <h1>{data.hi} Welcome to React-Meteor-Apollo-GraphQL</h1>
+        </div>
+    )
+};
+
+const hiQuery = gql `
+    {
+        hi
+    }
+`;
+
+export default graphql(
+    hiQuery
+)(App);
