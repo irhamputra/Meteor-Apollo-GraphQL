bc8940cc3c878677889d9f6e919706dfecb49529
Muhamad Irham Prasetyo <irhamputraprasetyo@gmail.com>
2018-01-30 01:01:19 +0100

    mapping data query from graphql

--- imports/startup/server/register-api.js
+++ imports/startup/server/register-api.js
@@ -1,20 +1,32 @@
 import {createApolloServer} from "meteor/apollo";
 import {makeExecutableSchema} from "graphql-tools";
-
-import ResolutionsSchema from '../../api/resolutions/Resolutions.graphql';
+import ResolutionsSchema from "../../api/resolutions/Resolutions.graphql";
 
 const testSchema = `
     type Query {
-        hi: String
+        hi: String 
+        resolutions: [Resolutions]
     }
 `;
 
-const typeDefs =[ testSchema, ResolutionsSchema ];
+const typeDefs = [testSchema, ResolutionsSchema];
 
 const resolvers = {
     Query: {
         hi(){
             return "Hello User!"
+        },
+        resolutions(){
+            return [
+                {
+                    _id: 7,
+                    name: "Learning Apollo GraphQL"
+                },
+                {
+                    _id: 9,
+                    name: "Connecting main feature from Frontend to Backend with Apollo GraphQL"
+                }
+            ]
         }
     }
 };
--- imports/ui/app.js
+++ imports/ui/app.js
@@ -3,16 +3,27 @@ import gql from "graphql-tag";
 import {graphql} from "react-apollo";
 
 const App = ({data}) => {
-    return(
-        <div>
-            <h1>{data.hi} Welcome to React-Meteor-Apollo-GraphQL</h1>
+    if(data.loading) return null;
+    return (
+        <div className="container">
+            <h1>{data.hi}</h1>
+            <h3>Welcome to Meteor-Apollo GraphQL</h3>
+            <ul>
+                {data.resolutions.map(resolution => (
+                    <li key={resolution._id}>{resolution.name}</li>
+                ))}
+            </ul>
         </div>
     )
 };
 
 const hiQuery = gql`
     {
-      hi
+        hi
+        resolutions {
+            _id
+            name
+        }
     }
 `;
 
